9ebfed8c1a0e0a8fe91554ec103ed5e47c0eed0b
MiguelTorralvo0020 <mtg0020@alu.medac.es>
2025-10-31 11:30:45 +0100

    creacionBarraNavegacionCssLoginyDemas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,15 @@ export const routes: Routes = [
   // Ruta para el componente de login
   path: '',
   component:LoginComponent,
-}, {
+},
+{
+
+  path: 'Login',
+  component:LoginComponent,
+},
+
+
+{
   path: 'citas',
   loadChildren: () =>
     import('./features/citas/routes').then(m => m.CITAS_ROUTES)
