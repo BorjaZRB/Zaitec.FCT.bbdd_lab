22c582434d54a3bc9f4a23a358ba9788226240f4
juanjo-tirado <jtf0003@alu.medac.es>
2025-11-05 13:34:52 +0100

    Creación pantalla Resultados-Analisis

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,32 +2,32 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login/login';
 import { CitasCalendarioComponent } from './features/citas/pages/calendario/citas-calendar/citas-calendario';
 import { CitasComponent } from './features/citas/pages/citas/citas.component';
+import { ResultadosAnalisis } from './features/resultados-analisis/resultados-analisis';
 
 export const routes: Routes = [
-{
-  // Ruta para el componente de login
-  path: '',
-  component:LoginComponent,
-},
-{
-
-  path: 'Login',
-  component:LoginComponent,
-  title:'Inicio'
-},
-
-
-{
-  path: 'citas',
-  component:CitasComponent,
-  title: 'Citas'
-},
-{ path: 'calendario',
-  component: CitasCalendarioComponent,
-  title: 'Calendario de Citas'
-},
-
-
-
-
+  {
+    // Ruta por defecto → login
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Inicio',
+  },
+  {
+    path: 'citas',
+    component: CitasComponent,
+    title: 'Citas',
+  },
+  {
+    path: 'calendario',
+    component: CitasCalendarioComponent,
+    title: 'Calendario de Citas',
+  },
+  {
+    path: 'resultados-analisis',
+    component: ResultadosAnalisis,
+    title: 'Resultados de Análisis',
+  },
 ];
