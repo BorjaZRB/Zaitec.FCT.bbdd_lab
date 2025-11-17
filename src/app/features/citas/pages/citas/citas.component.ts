92bd68b9fc1a3b64b7296a359a210ff7c7bacfac
Alfonso <agm0121@alu.medac.es>
2025-11-17 13:30:05 +0100

    Unificado el calendario con la lista de citas

--- src/app/features/citas/pages/citas/citas.component.ts
+++ src/app/features/citas/pages/citas/citas.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CitasListPage } from "../list/citas-list.page";
 import { Cita } from '../../types';
+import { CitasCalendarioComponent } from '../calendario/citas-calendar/citas-calendario';
+import { CitasAdd } from "../../components/citas-add/citas-add";
+import { CitaService } from '../../../../core/services/CitasService.service';
 
 @Component({
   selector: 'app-citas',
-  imports: [CitasListPage],
+  imports: [CitasListPage, CitasCalendarioComponent, CitasAdd],
   templateUrl: './citas.component.html',
   styleUrl: './citas.component.scss'
 })
 
 // 3. CLASE
 export class CitasComponent {
+
+  private citaService = inject(CitaService)
+
+    async onNuevaCita(cita: Omit<Cita, 'id_cita'>) {
+    await this.citaService.addCita(cita);
+    }
 
 
 }
